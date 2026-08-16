54e00ad2ee2122cbab7fe38df4795994403df3f6
aha5495 <151107011+aha5495@users.noreply.github.com>
2026-08-16 00:55:46 -0400

    Add files via upload

---
+++ DayOne_SpringBootGUI/src/main/java/com.example.SB_Day1Practice/SbDay1PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SB_Day1Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDay1PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDay1PracticeApplication.class, args);
+	}
+
+}
